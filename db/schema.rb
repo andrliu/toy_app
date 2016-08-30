c6290dfafe7ad7901590379327ee65f9760c5144
Andrew Liu <dieseliu321@gmail.com>
2016-08-30 00:28:16 -0700

    Finish toy app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830061740) do
+ActiveRecord::Schema.define(version: 20160830063857) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
